7a66a4137c76a1ed689616f5f884846354fbc977
tonytwei <tony.t.wei@gmail.com>
2025-12-25 23:15:46 +1300

    edit logo sizing

--- src/lib/Logo.svelte
+++ src/lib/Logo.svelte
@@ -1,14 +1,5 @@
-<svg
-	viewBox="0 0 100 100"
-	width="32"
-	height="32"
-	class="block"
-	aria-hidden="false"
-	role="img"
->
-	<g
-		transform={`translate(50, 50)`}
-	>
+<svg viewBox="0 0 100 100" width="32" height="32" class="block" aria-hidden="false" role="img">
+	<g transform={`translate(50, 50) scale(1.2)`}>
 		<!-- square -->
 		<g transform="rotate(240) translate(12)">
 			<rect
@@ -17,7 +8,7 @@
 				width="40"
 				height="40"
 				rx="3"
-				class="fill-zinc-900 dark:fill-zinc-100 stroke-zinc-100 dark:stroke-zinc-900 stroke-3"
+				class="fill-zinc-900 dark:fill-zinc-100 stroke-white dark:stroke-black stroke-3"
 			/>
 		</g>
 
@@ -30,7 +21,7 @@
 				points="28,0 -14,-24.249 -14,24.249"
 				fill="none"
 				stroke-linejoin="round"
-				class="stroke-zinc-100 dark:stroke-zinc-900 stroke-3"
+				class="stroke-white dark:stroke-black stroke-3"
 			/>
 		</g>
 
@@ -39,13 +30,7 @@
 			<circle cx="0" cy="0" r="21" class="fill-zinc-900 dark:fill-zinc-100" />
 		</g>
 		<g transform="rotate(120) translate(12)">
-			<circle
-				cx="0"
-				cy="0"
-				r="21"
-				fill="none"
-				class="stroke-zinc-100 dark:stroke-zinc-900 stroke-3"
-			/>
+			<circle cx="0" cy="0" r="21" fill="none" class="stroke-white dark:stroke-black stroke-3" />
 		</g>
 	</g>
 </svg>
